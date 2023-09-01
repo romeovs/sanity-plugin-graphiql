a57a19e90cb6cb1b6f70a49b0e1d112f4ccc8322
Romeo Van Snick <romeo@romeovansnick.be>
2023-09-01 13:24:54 +0200

    Move GraphiQL into separate component

--- src/component.tsx
+++ src/component.tsx
@@ -2,16 +2,13 @@ import * as React from 'react'
 
 import {Tool} from 'sanity'
 
-import {GraphiQL} from 'graphiql'
-
 import 'graphiql/graphiql.css'
 import './custom.css'
 
 import {GraphiQLToolConfig} from './types'
-import {useFetcher} from './use-fetcher'
-import {useTimedFetcher} from './use-timed'
 import {Header} from './header'
 import {useListGraphQLApis} from './use-list-graphql-apis'
+import {GraphiQL} from './graphiql'
 
 type GraphiQLToolProps = {
   tool: Tool<GraphiQLToolConfig>
@@ -23,23 +20,10 @@ export default function GraphiQLTool(props: GraphiQLToolProps) {
   const apis = useListGraphQLApis(version)
   const [url, setUrl] = React.useState<string | null>(null)
 
-  const _fetcher = useFetcher(url)
-  const [fetcher, elapsed] = useTimedFetcher(_fetcher)
-
-  if (!fetcher) {
-    return null
-  }
-
   return (
     <div className='graphiql-tool-wrapper'>
       <Header url={url} onUrlChange={setUrl} apis={apis} />
-      <GraphiQL fetcher={fetcher} defaultQuery={''}>
-        <GraphiQL.Footer>
-          <div className='graphiql-footer-elapsed'>
-            End-to-end: ${elapsed ? `${elapsed}ms` : 'n/a'}
-          </div>
-        </GraphiQL.Footer>
-      </GraphiQL>
+      <GraphiQL url={url} />
     </div>
   )
 }
---
+++ src/graphiql.tsx
@@ -0,0 +1,28 @@
+import {GraphiQL as Component} from 'graphiql'
+
+import {useFetcher} from './use-fetcher'
+import {useTimedFetcher} from './use-timed'
+
+type GraphiQLProps = {
+  url: string | null
+}
+
+export function GraphiQL(props: GraphiQLProps) {
+  const {url} = props
+  const _fetcher = useFetcher(url)
+  const [fetcher, elapsed] = useTimedFetcher(_fetcher)
+
+  if (!fetcher) {
+    return null
+  }
+
+  return (
+    <Component fetcher={fetcher} defaultQuery={''}>
+      <Component.Footer>
+        <div className='graphiql-footer-elapsed'>
+          End-to-end: ${elapsed ? `${elapsed}ms` : 'n/a'}
+        </div>
+      </Component.Footer>
+    </Component>
+  )
+}
